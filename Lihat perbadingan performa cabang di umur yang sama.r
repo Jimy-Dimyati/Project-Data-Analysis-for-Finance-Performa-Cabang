2c7e3a514f75f35ac8d21cc80a8d3c983282fa17
Jimy-Dimyati <113796534+Jimy-Dimyati@users.noreply.github.com>
2024-05-20 02:16:20 +0700

    Lihat perbadingan performa cabang di umur yang sama.r

---
+++ Lihat perbadingan performa cabang di umur yang sama.r
@@ -0,0 +1,14 @@
+library(dplyr)
+library(scales)
+df_loan_mei_flag %>% 
+  filter(umur == 3) %>% 
+  inner_join(df_loan, by = 'cabang') %>% 
+  filter(tanggal_cair >= '2020-05-01', tanggal_cair <= '2020-05-31') %>% 
+  group_by(cabang, flag) %>% 
+  summarise(jumlah_hari = n_distinct(tanggal_cair),
+            agen_aktif = n_distinct(agen),
+            total_loan_cair = n_distinct(loan_id),
+            avg_amount = mean(amount), 
+            total_amount = sum(amount)) %>% 
+  arrange(total_amount) %>% 
+  mutate_if(is.numeric, funs(comma))
