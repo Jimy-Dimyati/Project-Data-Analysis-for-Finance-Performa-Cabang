5d1d30f647cf0fb4c0e42f46273eb5edffdb8643
Jimy-Dimyati <113796534+Jimy-Dimyati@users.noreply.github.com>
2024-05-20 02:14:31 +0700

    Plot relasi umur dan performa mei.r

---
+++ Plot relasi umur dan performa mei.r
@@ -0,0 +1,8 @@
+library(ggplot2)
+
+ggplot(df_loan_mei_umur, aes(x = umur, y = total_amount)) +
+  geom_point() +
+  scale_y_continuous(labels = scales::comma) +
+  labs(title = "Semakin berumur, perfoma cabang akan semakin baik",
+       x = "Umur (bulan)",
+       y = "Total Amount")
